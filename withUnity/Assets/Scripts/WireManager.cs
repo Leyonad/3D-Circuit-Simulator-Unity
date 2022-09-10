c26f69f713ef054f4f6502ce152723ebf3861d38
Leyonad <78915530+Leyonad@users.noreply.github.com>
2022-09-10 11:59:08 +0200

    tasks finished:
    draw line to mouse when clicking on a metal; cancel on right click

--- withUnity/Assets/Scripts/WireManager.cs
+++ withUnity/Assets/Scripts/WireManager.cs
@@ -1,45 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WireManager : MonoBehaviour
 {
+    [SerializeField] 
+    public static Camera cam;
     public static Material wireMaterial;
     public int numCapVertices = 4;
 
     private void Start()
     {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         wireMaterial = Resources.Load("Materials/Wire_Material", typeof(Material)) as Material;
-        Debug.Log(wireMaterial);
+        if (wireMaterial == null)
+            Debug.Log("Wire Material not found!");
+    }
+
+    private void Update()
+    {
+        if (Wire.justCreated != null)
+        {
+            Wire.justCreated.wireFollowMouse(Wire.justCreated);
+
+            //cancel creation of a new wire by destroying it
+            if (Mouse.current.rightButton.wasPressedThisFrame)
+            {
+                Destroy(Wire.justCreated.lineObject);
+                Wire.justCreated = null;
+            }
+            //finish creation of a new wire by adding it to _registry
+            else if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                //Check if wire doesnt already exist
+                //
+
+                Wire._registry.Add(Wire.justCreated);
+                Wire.justCreated = null;
+            }
+        }
     }
 
     public class Wire
     {
         public static List<Wire> _registry = new List<Wire>();
-        public Vector3 pos1;
-        public Vector3 pos2;
-        GameObject newWire;
+        public List<Vector3> verticesOfWire = new List<Vector3>();
+        public GameObject lineObject;
+        LineRenderer lineRenderer;
+        public static Wire justCreated;
 
         public Wire(GameObject startObject)
-        {
-            pos1 = startObject.transform.position;
-            pos2 = new Vector3(5f, 5f, 5f);
+        {   
+            //Later do this in a loop
+            verticesOfWire.Add(startObject.transform.position);
+            verticesOfWire.Add(new Vector3(5, 5, 5));
 
-            newWire = new GameObject($"wire{_registry.Count+1}");
-            createLineRenderer();
-
-            _registry.Add(this);
-            Debug.Log("New Wire(): pos1 = " + pos1 + " pos2 = " + pos2);
+            createLineObject();
+            justCreated = this;
         }
 
-        private void createLineRenderer()
+        public void wireFollowMouse(Wire justCreated)
         {
-            LineRenderer lineRenderer = newWire.AddComponent<LineRenderer>();
+            Vector3 mouseWorld = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            justCreated.lineRenderer.SetPosition(verticesOfWire.Count-1, mouseWorld);
+
+            //update the other vertices of the line based on the start and end position
+            //with kinetic equation (Sebastian Lague Video)
+
+        }
+
+        private void createLineObject()
+        {
+            lineObject = new GameObject($"wire{_registry.Count + 1}");
+            lineRenderer = lineObject.AddComponent<LineRenderer>();
             lineRenderer.material = wireMaterial;
             lineRenderer.widthMultiplier = 0.1f;
             lineRenderer.numCapVertices = 4;
-            lineRenderer.SetPosition(0, pos1);
-            lineRenderer.SetPosition(1, pos2);
+            
+            int i = 0;
+            foreach (Vector3 pos in verticesOfWire)
+            {
+                lineRenderer.SetPosition(i, pos);
+                i++;
+            }
+            Debug.Log("New Wire created");
         }
     }
 
